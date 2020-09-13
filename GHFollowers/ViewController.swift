84d8ed753f6c463e8558b902f59723bf64dad5e2
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-09-13 20:17:59 +1000

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Bogdan on 13/9/20.
+//  Copyright © 2020 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
